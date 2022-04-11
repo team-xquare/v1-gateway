af97024e09d291deaa3911ab30e49d0b8892818c
jhhong0509 <jhhong0509@naver.com>
2022-04-11 12:05:26 +0900

    chore: add classes for dependency management

---
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -0,0 +1,12 @@
+object Dependencies {
+    const val SPRING_VALIDATION = "org.springframework.boot:spring-boot-starter-validation"
+    const val WEBFLUX = "org.springframework.boot:spring-boot-starter-webflux"
+    const val KOTLIN_JACKSON = "com.fasterxml.jackson.module:jackson-module-kotlin"
+    const val COROUTINE_REACTOR_EXTENSION = "io.projectreactor.kotlin:reactor-kotlin-extensions"
+    const val COROUTINE_REACTOR = "org.jetbrains.kotlinx:kotlinx-coroutines-reactor"
+    const val CIRCUIT_BREAKER = "org.springframework.cloud:spring-cloud-starter-circuitbreaker-reactor-resilience4j"
+    const val SPRING_CLOUD_GATEWAY = "org.springframework.cloud:spring-cloud-starter-gateway"
+    const val KOTLIN_REFLECT = "org.jetbrains.kotlin:kotlin-reflect"
+    const val KOTLIN_STDLIB = "org.jetbrains.kotlin:kotlin-stdlib-jdk8"
+    const val SPRING_BOOT_TEST = "org.springframework.boot:spring-boot-starter-test"
+}
