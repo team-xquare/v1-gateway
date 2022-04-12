ef02b12dcc4fe885c938c6aab340c440ef6d5ed1
jhhong0509 <jhhong0509@naver.com>
2022-04-12 12:12:05 +0900

    feat: add InvalidJwtException

--- src/main/kotlin/com/xquare/gateway/apigateway/configuration/authentication/exception/InvalidJwtException.kt
+++ src/main/kotlin/com/xquare/gateway/apigateway/configuration/authentication/exception/InvalidJwtException.kt
@@ -1,4 +1,11 @@
 package com.xquare.gateway.apigateway.configuration.authentication.exception
 
-class InvalidJwtException {
+import com.xquare.gateway.apigateway.configuration.exceptions.BaseException
+
+class InvalidJwtException(
+    errorMessage: String
+): BaseException(errorMessage, 401) {
+    companion object {
+        const val JWT_EXPIRED_MESSAGE = "Jwt Token Expired"
+    }
 }
